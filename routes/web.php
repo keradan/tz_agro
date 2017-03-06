da8509773f546443dad37ad2ab2ea47d91a5f441
Daniel Kerasidi <kerasidydan@gmail.com>
2017-03-07 01:02:38 +0200

    testing router 4

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
+/**
+ * Переадресация с адресов в которых присутствует 'public' или 'index.php'
+ */
 if (strpos($_SERVER['REQUEST_URI'], 'public') !== false || strpos($_SERVER['REQUEST_URI'], 'index.php') !== false) {
-	
-	var_dump($_SERVER['HTTPS']);
-	echo "<br>";
-	var_dump($_SERVER['HTTP_HOST']);
-	echo "<br>";
-	var_dump($_SERVER['REQUEST_URI']);
-	echo "<br>";
-	echo "<br>";
-	echo "redirect: " . (($_SERVER['HTTPS'])? "https://" : "http://") . $_SERVER['HTTP_HOST'];
-	die('ok');	
+	header('Location: http://' . $_SERVER['HTTP_HOST']);
+	die();	
 }
-
-
-
 
 Route::get('/', function () {
     return view('welcome');
