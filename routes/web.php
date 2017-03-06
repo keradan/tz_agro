07d6c442b4e4f5541a42a1ee5448f87fda8e9c05
Daniel Kerasidi <kerasidydan@gmail.com>
2017-03-07 00:15:18 +0200

    testing router

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return ['some' => 'text'];
+});
+
+Route::get('/public', function () {
+    return "This is public"
+});
