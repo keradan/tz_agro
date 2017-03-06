4574a18c270a9a48c341978937f0dd4aa4bc351c
Daniel Kerasidi <kerasidydan@gmail.com>
2017-03-07 00:55:58 +0200

    testing router 3

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,18 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-echo "<pre>";
-var_dump($_SERVER);
+
 
 if (strpos($_SERVER['REQUEST_URI'], 'public') !== false || strpos($_SERVER['REQUEST_URI'], 'index.php') !== false) {
+	
+	var_dump($_SERVER['HTTPS']);
+	echo "<br>";
+	var_dump($_SERVER['HTTP_HOST']);
+	echo "<br>";
 	var_dump($_SERVER['REQUEST_URI']);
+	echo "<br>";
+	echo "<br>";
+	echo "redirect: " . (($_SERVER['HTTPS'])? "https://" : "http://") . $_SERVER['HTTP_HOST'];
 	die('ok');	
 }
 
