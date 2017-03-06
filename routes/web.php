10c8990247029541b74d0334c30d39dc5e32272f
Daniel Kerasidi <kerasidydan@gmail.com>
2017-03-07 00:42:08 +0200

    testing router 2

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+echo "<pre>";
+var_dump($_SERVER);
+
+if (strpos($_SERVER['REQUEST_URI'], 'public') !== false || strpos($_SERVER['REQUEST_URI'], 'index.php') !== false) {
+	var_dump($_SERVER['REQUEST_URI']);
+	die('ok');	
+}
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
