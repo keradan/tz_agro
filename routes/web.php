c5494c04b22e3838e8b00c0bb101ab2c1a733b2a
Daniel Kerasidi <kerasidydan@gmail.com>
2017-03-07 21:36:28 +0200

    first kirasin commit to testing my gitflow

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/testdb', function () {
     $users = App\User::get();
     return $users;
 });
+
+Route::get('/test/branches', function () {
+    return "If you see this it is a good sign that all is okey now";
+});
